ba062b525b4d60cba1a4cef3c9a77cdc2aaf9e65
duythong2505@gmail.com <duythong2505@gmail.com>
2023-09-10 21:19:27 +0700

    Fix folder of TwoPlayerMode_Screen

--- src/snakepredation/FXML_Folder/TwoPlayerMode_Screen/TwoPlayerMode_ScreenController.java
+++ src/snakepredation/FXML_Folder/TwoPlayerMode_Screen/TwoPlayerMode_ScreenController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
  */
-package TwoPlayerMode_Screen;
+package snakepredation.FXML_Folder.TwoPlayerMode_Screen;
 
 import java.net.URL;
 import java.util.ResourceBundle;
